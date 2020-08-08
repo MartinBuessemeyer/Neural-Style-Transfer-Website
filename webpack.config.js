74c3849b044623b599f042835ca0fa86a5fac7ba
Martin Buessemeyer <Martin.Buessemeyer@student.hpi.de>
2020-08-08 19:59:18 +0200

    initial setup for webpack, react, typescript

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isDevelopment = true;
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
+            {
+                    test: /\.module\.s(a|c)ss$/,
+            loader: [
+              isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true,
+                      sourceMap: isDevelopment
+                }
+          },
+          {
+            loader: 'sass-loader',
+                options: {
+                  sourceMap: isDevelopment
+                }
+          }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+            exclude: /\.module.(s(a|c)ss)$/,
+            loader: [
+              isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+              'css-loader',
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: isDevelopment
+                }
+          }
+            ]
+        }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+            chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+        }),
+    ],
+    mode: 'development',
+    devtool: "source-map",
+    resolve: {
+        extensions: [".js", ".ts", ".tsx", ".scss"]
+    }
+}
